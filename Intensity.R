28c74cc81e89696b530470a09c3821220579b33c
JianhengZhao <96509813+JianhengZhao@users.noreply.github.com>
2024-02-06 15:09:07 -0500

    Update Intensity.R

--- Intensity.R
+++ Intensity.R
@@ -4,12 +4,11 @@ library(stats4)
 library(splines)
 library(VGAM)
 
-
 # setting dictionary file
-Data_Clean <- read.csv("Data/Data_Clean.csv",check.names=FALSE)
+Data_Clean_V1 <- read.csv("Data/Data_Clean_V1.csv",check.names=FALSE)
 
 #sawlog intensity
-Data_Saw<-subset(Data_Clean, ratioSawlog>0&ratioSawlog<0.7)
+Data_Saw<-subset(Data_Clean_V1, ratioSawlog>0&ratioSawlog<0.7)
 I1 <- vglm(ratioSawlog ~ L_BioSaw+I(L_BioSaw^2)+postGrowth+I(postGrowth^2), tobit(Lower = 0, Upper = 0.7, type.f = "cens"), data = Data_Saw, control = vglm.control(epsilon = 1e-6))
 ## Sampled data
 Data_Saw$Sawintensity<-predict(I1, newdata=Data_Saw, type="response")
@@ -19,16 +18,16 @@ with(Data_Saw, cor(Sawintensity, ratioSawlog))
 scatter.smooth(x=Data_Saw$Sawintensity, Data_Saw$ratioSawlog,col="red", main="Growth")
 
 ## All data
-Data_Clean$Sawintensity<-predict(I1, newdata=Data_Clean, type="response")
-Data_Clean2<-subset(Data_Clean, ratioSawlog>0&ratioSawlog<0.7)
-with(Data_Clean2, cor(Sawintensity, ratioSawlog))
+Data_Clean_V1$Sawintensity<-predict(I1, newdata=Data_Clean_V1, type="response")
+Data_Clean_V1<-subset(Data_Clean_V1, ratioSawlog>0&ratioSawlog<0.7)
+with(Data_Clean_V1, cor(Sawintensity, ratioSawlog))
 
 
 
 #Pulp intensity
-Data_Pulp<-subset(Data_Clean, ratioPulp>0&ratioPulp<0.7)
-summary(I2 <- vglm(ratioPulp ~  PriceSaw_county+PricePulp_county
-                   +L_BioTot +BioPulpLD+postGrowthp+postGrowth_sqrp+Conservation_Type, tobit(Lower = 0, Upper = 0.7, type.f = "cens"), data = Data_Pulp))
+Data_Pulp<-subset(Data_Clean_V1, ratioPulp>0&ratioPulp<0.7)
+summary(I2 <- vglm(ratioPulp ~  PriceSaw_county+PricePulp_county+
+                   L_BioTot +BioPulpLD+postGrowthp+postGrowth_sqrp+Conservation_Type, tobit(Lower = 0, Upper = 0.7, type.f = "cens"), data = Data_Pulp))
 
 ## Sampled data
 Data_Pulp$Pulpintensity<-predict(I2, newdata=Data_Pulp, type="response")
@@ -38,18 +37,12 @@ with(Data_Pulp, cor(Pulpintensity, ratioPulp))
 
 
 ## All data
-Data_Clean$Pulpintensity<-predict(I2, newdata=Data_Clean, type="response")
-Data_Clean2<-subset(Data_Clean, ratioPulp>0&ratioPulp<0.7)
-with(Data_Clean2, cor(Pulpintensity, ratioPulp))
-scatter.smooth(x=Data_Clean$Pulpintensity, Data_Clean$ratioPulp,col="red", main="Growth")
+Data_Clean_V1$Pulpintensity<-predict(I2, newdata=Data_Clean_V1, type="response")
+Data_Clean_V1<-subset(Data_Clean_V1, ratioPulp>0&ratioPulp<0.7)
+with(Data_Clean_V1, cor(Pulpintensity, ratioPulp))
+scatter.smooth(x=Data_Clean_V1$Pulpintensity, Data_Clean_V1$ratioPulp,col="red", main="Growth")
 
-# Output
-write.csv(Data_Clean, "Data_CleanV2.csv")
-
-
-
-
-
+write.csv(Data_Clean_V1, "Data_Clean_V2.csv")
 
 
 
