cc460337f8ea50606c44c1960bf908e14850dbbf
JianhengZhao <96509813+JianhengZhao@users.noreply.github.com>
2024-02-12 12:36:44 -0500

    Update 1_Data_Clean.R

--- 1_Data_Clean.R
+++ 1_Data_Clean.R
@@ -4,13 +4,6 @@ setwd("")
 # Input data
 Data_V0 <- read.csv("Data_V0.csv",check.names=FALSE)
 
-## Convert dependent variables to factors 
-columns <- c("ChoiceSawlog", "ChoicePulp", "ChoiceLD", "ChoiceTot")
-for (col in columns) {
-  Data_V0[[col]] <- as.factor(Data_V0[[col]])
-  levels(Data_V0[[col]]) <- c("None", "Partial", "Final")
-}
-
 ## Distance conversion to kilometers 
 distance_columns <- c("Distance.roadsegment.", "Distance.Rail.", "Distance.national_highway.", "Distance.State.road.")
 for (col in distance_columns) {
@@ -24,7 +17,7 @@ Data_V0$AVG_val_ac<-Data_V0$AVG_val_ac*2.47
 
 ## Conservation Land
 Data_V0$Conservation_Type<-as.factor(Data_V0$Conservation_Type )
-levels(Data_V0$Conservation_Type ) <- c("non-conserved", "Private", "Public")
+levels(Data_V0$Conservation_Type ) <- c("non-conserved","Private","Public")
 
 ## Other conversion 
 Data_V0$County<-as.factor(Data_V0$County)
