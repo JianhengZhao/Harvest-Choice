5419950954bc90963f22ed6228a8363fb061c26c
JianhengZhao <96509813+JianhengZhao@users.noreply.github.com>
2024-02-12 12:42:42 -0500

    Update 4_Choice.R

--- 4_Choice.R
+++ 4_Choice.R
@@ -1,14 +1,18 @@
+install.packages("stargazer")
+library("stargazer")
 library("nnet") 
+
+# Input data
 Data_Clean_V2 <- read.csv("Data_Clean_V2.csv",check.names=FALSE)
 
-# Convert dependent variables to factors 
+## Convert dependent variables to factors 
 columns <- c("ChoiceSawlog", "ChoicePulp", "ChoiceLD", "ChoiceTot")
 for (col in columns) {
   Data_Clean_V2[[col]] <- as.factor(Data_Clean_V2[[col]])
   levels(Data_Clean_V2[[col]]) <- c("None", "Partial", "Final")
 }
 
-# Relevel the factor to make 'None' the reference level
+## Relevel the factor to make 'None' the reference level
 columns <- c("ChoiceSawlog", "ChoicePulp", "ChoiceLD", "ChoiceTot")
 for (col in columns) {
   new_col_name <- paste0(col, "0") # Create a new column name by appending '0'
@@ -16,27 +20,29 @@ for (col in columns) {
 }
 
 #************************************#
-m <- multinom(ChoiceSawlog0  ~ PriceSaw_county + PricePulp_county  + L_BioTot + BioTot +postGrowth+postGrowth_sqr+SWMills +AVG_val_ac +Distance.national_highway.+County +Conservation_Type+ELEV+Year+Coastal, data =Data_Clean_V2,na.action = na.exclude )
-stargazer(m, title="Sawlog Combined", align=TRUE, dep.var.labels=c("Thinning","Final"),
-          no.space=TRUE, type="text", out="m.htm")
+S <- multinom(ChoiceSawlog0  ~ PriceSaw_county + PricePulp_county  + L_BioTot + BioTot +postGrowth+postGrowth_sqr+SWMills +AVG_val_ac +Distance.national_highway.+County +Conservation_Type+ELEV+Year+Coastal, data =Data_Clean_V2,na.action = na.exclude )
+stargazer(S, title="Sawlog Combined", align=TRUE, dep.var.labels=c("Thinning","Final"),
+          no.space=TRUE, type="text", out="Sawlog.htm")
 
 #************************************#
-p <-multinom(ChoicePulp0  ~ PriceSaw_county + PricePulp_county +L_BioTot +BioPulpLD+postGrowthp+postGrowth_sqrp+PulpMills+AVG_val_ac+Distance.national_highway.+County+Conservation_Type +ELEV+Year+Coastal, data =Data_Clean_V2,na.action = na.exclude )
 
-stargazer(p, title="pulpwood Combined", align=TRUE, dep.var.labels=c("Thinning","Final"),
-          no.space=TRUE, type="text", out="p1.htm")
+P <-multinom(ChoicePulp0  ~ PriceSaw_county + PricePulp_county +L_BioTot +BioPulpLD+postGrowthp+postGrowth_sqrp+PulpMills+AVG_val_ac+Distance.national_highway.+County+Conservation_Type +ELEV+Year+Coastal, data =Data_Clean_V2,na.action = na.exclude )
 
-# Validation
+stargazer(P, title="pulpwood Combined", align=TRUE, dep.var.labels=c("Thinning","Final"),
+          no.space=TRUE, type="text", out="Pulplog.htm")
+
+
+#### Validation
 ## Deviance, log-likelihood and AIC
-deviance(p)
-logLik(p)
-AIC(p)
+deviance(P)
+logLik(P)
+AIC(P)
 ## Log-likelihoods for full model and 0-model without predictors ##
-m0<-multinom(ChoiceSawlog0  ~ 1, data =Data_Clean_V2,na.action = na.exclude)
-LLf<-logLik(m)
-LL0<-logLik(m0)
-p0<-multinom(ChoicePulp0  ~ 1, data =Data_Clean_V2,na.action = na.exclude)
-LLf<-logLik(p)
-LL0<-logLik(p0)
+S0<-multinom(ChoiceSawlog0  ~ 1, data =mydata,na.action = na.exclude)
+LLf<-logLik(S)
+LL0<-logLik(S0)
+P0<-multinom(ChoicePulp0  ~ 1, data =mydata,na.action = na.exclude)
+LLf<-logLik(P)
+LL0<-logLik(P0)
 ## McFadden pseudo-R2
 as.vector(1 - (LLf / LL0))
